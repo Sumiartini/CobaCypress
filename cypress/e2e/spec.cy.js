02c7224ae52064ecfa340154b811422688f6f710
unknown <sumiartinisrirahayu@gmail.com>
2023-12-30 06:52:38 +0700

    [CAT-001] Skenario Login

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,7 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://dev-pos.nolimitz.id/') //visit website
+    cy.get('input[name=email]').type("cari.kopi@mailinator.com") //input email
+    cy.get('input[name=password]').type(`${"password."}{enter}`) //input password dan klik enter untuk masuk
+  })
+})
